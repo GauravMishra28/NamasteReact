0cd2c513185c3ed8321155c188e50e05892d2837
Gaurav Mishra <gauravmemishra@gmail.com>
2023-01-11 22:34:05 +0530

    secondpush

--- auth/index.js
+++ auth/index.js
@@ -1,58 +1,17 @@
 const express = require("express");
 const { connection } = require("./config/db");
-const { AuthModel } = require("./models/User.model");
-
+const { userRouter } = require("./routes/User.route");
 const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Welcome");
+  res.send("Welcome to Home Page");
 });
 
-app.post("/registration",(req,res)=>{
-    const payload= req.body
-    try{
-    const user= new AuthModel(payload)
-    user.save()
-    res.send("User Registered")
-    }
-    catch(err){
-        res.send("Error in Registering the user")
-        console.log(err)
-    }
-  
-})
 
-app.post("/login",(req,res)=>{
-    const {email,pass}=req.body
-    try{
-        let user= AuthModel.findOne({email,pass})
-        if(user.length>0)
-        {
-            res.send("Login Successful")
-        }
-    }
-    catch(err){
-        res.send("Error in Login")
-        console.log(err)
-    }
-    
+app.use("/users",userRouter)
 
-  
-})
-
-app.get("/about",(req,res)=>{
-    res.send("About page")
-})
-
-
-app.get("/contacts",(req,res)=>{
-    res.send("Contacts page")
-})
-
-
-
-app.listen(4500, async() => {
+app.listen(4500, async () => {
   try {
     await connection;
     console.log("connected to db");
---
+++ auth/routes/User.route.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { AuthModel } = require("../models/User.model");
+const userRouter = express.Router();
+
+userRouter.post("/register", async (req, res) => {
+  const { email, pass, age, name } = req.body;
+  try {
+    bcrypt.hash(pass, 5, async (err, secure_password) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const user = new AuthModel({ email, pass: secure_password, age, name });
+        await user.save();
+        res.send("User Registered");
+      }
+    });
+  } catch (err) {
+    res.send("Error in Registering the user");
+    console.log(err);
+  }
+});
+
+userRouter.post("/login", async (req, res) => {
+  const { email, pass } = req.body;
+  try {
+    let user = await AuthModel.find({ email });
+    if (user.length > 0) {
+      bcrypt.compare(pass, user[0].pass, (err, result) => {
+        if (result) {
+          const token = jwt.sign({ auth: "yes" }, "masai");
+          res.send({ msg: "Login Successful", token: token });
+        } else {
+          {
+            res.send("Wrong Credentials");
+          }
+        }
+      });
+    } else {
+      res.send("Wrong Credentials");
+    }
+  } catch (err) {
+    res.send("Error in Login");
+    console.log(err);
+  }
+});
+
+
+module.exports={userRouter}
